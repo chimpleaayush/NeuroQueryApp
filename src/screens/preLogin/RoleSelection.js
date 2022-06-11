92688e12dc8888202f2a77feb956a8a5752e6f9a
Vicky Keshri <vicky@mplgaming.com>
2022-06-11 22:13:12 +0530

    Remove unwanted code

--- src/screens/preLogin/RoleSelection.js
+++ src/screens/preLogin/RoleSelection.js
@@ -1,25 +1,27 @@
-
 import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, ImageBackground} from 'react-native';
-import { Color } from '../../asset';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
+import {Color} from '../../asset';
 const RoleSelection = ({navigation}) => {
-  const navigateToLogin=()=>{
-    navigation.navigate('Login')
-  }
+  const navigateToLogin = () => {
+    navigation.navigate('Login');
+  };
   return (
     <ImageBackground source={null} style={styles.container}>
-          <Text style={styles.login}>Log/Register in As</Text>
-        <TouchableOpacity style={styles.button}  onPress={navigateToLogin}>
-          <Text style={styles.text}>Student</Text>
-        </TouchableOpacity>
-      
+      <Text style={styles.login}>Log/Register in As</Text>
+      <TouchableOpacity style={styles.button} onPress={navigateToLogin}>
+        <Text style={styles.text}>Student</Text>
+      </TouchableOpacity>
 
-      <TouchableOpacity style={styles.button}  onPress={navigateToLogin}>
-          <Text style={styles.text}>Mentor</Text>
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={navigateToLogin}>
+        <Text style={styles.text}>Mentor</Text>
+      </TouchableOpacity>
 
-        {/* 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 */}
-      
     </ImageBackground>
   );
 };
@@ -27,21 +29,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
   },
-  text:{
+  text: {
     fontSize: 16,
-    fontWeight:'bold',
+    fontWeight: 'bold',
     color: Color.blueText,
   },
-  login:{fontSize: 24,
-    fontWeight:'bold',},
-  button:{
-backgroundColor: Color.bgWhite,
-paddingHorizontal:20,
-paddingVertical:10,
-borderRadius: 5
-  }
-  
+  login: {fontSize: 24, fontWeight: 'bold'},
+  button: {
+    backgroundColor: Color.bgWhite,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
 });
 export default RoleSelection;
