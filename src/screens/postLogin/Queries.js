f44dcbf0b8f4f06a6dfeacda32c4ffcc0858e28c
Aayush <chimple.aayush@gmail.com>
2022-06-11 22:14:49 +0530

    adding skeleton

--- src/screens/postLogin/Queries.js
+++ src/screens/postLogin/Queries.js
@@ -1,17 +1,55 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-const Queries = () => {
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {Color} from '../../asset';
+
+const Queries = ({isStudent = false, queries = []}) => {
+  const Query = ({item, index}) => {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: Color.bgWhite,
+          paddingVertical: 5,
+          paddingHorizontal: 5,
+          borderRadius: 5,
+          flex: 1,
+          justifyContent: 'space-between',
+        }}>
+        <View
+          style={{
+            alignItems: 'flex-start',
+            justifyContent: 'space-between',
+            width: '80%',
+          }}>
+          <Text>title</Text>
+          <Text ellipsizeMode="tail" numberOfLines={2}>
+            descriptionkjjhhhhhhhhhhhhhhhhhhhhhhgbkhbghbghjbghjbgjbgjbgjbggjbhjbgjbjgbjhgbjbg
+          </Text>
+        </View>
+        <View style={{justifyContent: 'flex-start', alignItems: 'flex-end'}}>
+          <Text>time stamp</Text>
+        </View>
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Basic screen Queries</Text>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={[0, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+        renderItem={Query}
+        ItemSeparatorComponent={() => (
+          <View style={{backgroundColor: 'transparent', height: 2}} />
+        )}
+        keyExtractor={({item, index}) => index + ''}
+      />
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 5,
   },
 });
 export default Queries;
--- src/screens/postLogin/Tickets.js
+++ src/screens/postLogin/Tickets.js
@@ -1,17 +1,55 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-const Tickets = () => {
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {Color} from '../../asset';
+
+const Tickets = ({isStudent = false, tickets = []}) => {
+  const Ticket = ({item, index}) => {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: Color.bgWhite,
+          paddingVertical: 5,
+          paddingHorizontal: 5,
+          borderRadius: 5,
+          flex: 1,
+          justifyContent: 'space-between',
+        }}>
+        <View
+          style={{
+            alignItems: 'flex-start',
+            justifyContent: 'space-between',
+            width: '70%',
+          }}>
+          <Text>Ticket No. 1234</Text>
+          <Text ellipsizeMode="tail" numberOfLines={2}>
+            descriptionkjjhh
+          </Text>
+        </View>
+        <View style={{justifyContent: 'flex-start', alignItems: 'flex-end'}}>
+          <Text>Status: Pending</Text>
+        </View>
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Basic screen Tickets</Text>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+        renderItem={Ticket}
+        ItemSeparatorComponent={() => (
+          <View style={{backgroundColor: 'transparent', height: 2}} />
+        )}
+        keyExtractor={({item, index}) => index + ''}
+      />
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 5,
   },
 });
 export default Tickets;
